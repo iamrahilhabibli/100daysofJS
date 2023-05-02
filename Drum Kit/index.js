e61ca8c3a7996c9454597569b3721f71db7b39d4
= <=>
2023-05-03 01:47:41 +0400

    added audio to buttons

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,15 +1,17 @@
 function btnClick(){
     alert("I got clicked!");
 }
+var audio = new Audio("./sounds/tom-1.mp3");
 
+function playSound(){
+    audio.play();
+}
 
 
 var btns = document.querySelectorAll(".drum").length;
 
 
 for (let i = 0; i < btns; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click",btnClick);
+    document.querySelectorAll(".drum")[i].addEventListener("click",playSound);
+
 }
-// [...btns].forEach(btn => {
-//   addEventListener("click",btnClick);
-// });
