3393a805a7ad134781039ebc172a418bb8de1444
= <=>
2023-05-03 04:24:26 +0400

    random generator improved script

--- RandomHexColorGenerator/script.js
+++ RandomHexColorGenerator/script.js
@@ -1,6 +1,3 @@
-const alphabet = "abcdef"
-const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
+const random = Math.random().toString(16);
 
-const randomNumber = Math.floor(Math.random() * 9 + 1);
-console.log(randomNumber);
-
+console.log(random);
