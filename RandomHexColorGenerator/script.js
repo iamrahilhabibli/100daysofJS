5301db346e08a5c8d00d70377ad697a6dd9a732b
= <=>
2023-05-03 04:32:43 +0400

    script done

--- RandomHexColorGenerator/script.js
+++ RandomHexColorGenerator/script.js
@@ -1,3 +1,9 @@
-const random = Math.random().toString(16).substring(2,8);
 
-console.log(random);
+
+const randomColour = document.querySelector(".count");
+
+document.querySelector(".generate").addEventListener("click", () => {
+  const random = Math.random().toString(16).substring(2,8);
+  document.body.style.backgroundColor = "#" + random;
+  randomColour.innerHTML = "#" + random;
+})
