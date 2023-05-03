f2a53ae2969f1dafb7f5643b2e31b7d2b3055f9c
= <=>
2023-05-03 04:18:46 +0400

    random character and numbervariables added

---
+++ RandomHexColorGenerator/script.js
@@ -0,0 +1,6 @@
+const alphabet = "abcdef"
+const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
+
+const randomNumber = Math.floor(Math.random() * 9 + 1);
+console.log(randomNumber);
+
